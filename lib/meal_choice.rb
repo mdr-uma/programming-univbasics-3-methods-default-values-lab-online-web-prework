350b9175c24eac22a7df855bca827cc0af398efd
helpful-animation-4719 <um_2003_8@hotmail.com>
2019-09-26 17:19:43 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -7,9 +7,9 @@
 # meal_choice("broccoli", "macaroni")
 
 def meal_choice(veg1, veg2, protein = "meat")
+ puts "What a nutritious meal!"
  puts "A plate of #{protein} with #{veg1} and #{veg2}."
  meal_choice = "broccoli", "macaroni"
-
- end
+end
 
 meal_choice("broccoli", "macaroni")
