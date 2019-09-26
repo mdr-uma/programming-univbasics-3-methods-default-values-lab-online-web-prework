9225210e75fa324c726a4590a6364d3891781fd3
helpful-animation-4719 <um_2003_8@hotmail.com>
2019-09-26 17:30:53 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,16 @@
 # Your code here
+# def meal_choice(veg1, veg2, protein = "meat")
+#  puts "What a nutritious meal!"
+#  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+#  end
+#
+# meal_choice("broccoli", "macaroni")
+
+def meal_choice(veg1, veg2, protein = "meat")
+ puts "What a nutritious meal!"
+ puts "A plate of #{protein} with #{veg1} and #{veg2}."
+ meal_choice = "broccoli", "macaroni"
+ return "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+
+meal_choice("broccoli", "macaroni")
